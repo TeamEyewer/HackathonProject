2302805520d35a5008fe6d5dbcbfda316f0377f5
Dinuka <dinuka@eyewer.com>
2016-05-06 23:54:48 +0530

    Created Parent Layout for map

--- app/src/main/java/hackathon/eyewer/com/hackathonproject/MainViews/Dashboard.java
+++ app/src/main/java/hackathon/eyewer/com/hackathonproject/MainViews/Dashboard.java
@@ -1,7 +1,10 @@
 package hackathon.eyewer.com.hackathonproject.MainViews;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 import hackathon.eyewer.com.hackathonproject.R;
 
@@ -11,5 +14,12 @@ public class Dashboard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        ActionBar actionBar = Dashboard.this.getSupportActionBar();
+        actionBar.hide();
+
+        Window window = getWindow();
+        window.setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
     }
 }
