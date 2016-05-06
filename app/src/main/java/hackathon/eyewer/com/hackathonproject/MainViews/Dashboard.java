488a983b1f703a2ff1feb91b5ebf29bc067b7510
Dinuka <dinuka@eyewer.com>
2016-05-06 18:16:31 +0530

    Initial commit

---
+++ app/src/main/java/hackathon/eyewer/com/hackathonproject/MainViews/Dashboard.java
@@ -0,0 +1,15 @@
+package hackathon.eyewer.com.hackathonproject.MainViews;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import hackathon.eyewer.com.hackathonproject.R;
+
+public class Dashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+}
